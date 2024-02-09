c68807883689c58118f0ec4415f36b289192dc91
TetianaL <4215654@gmail.com>
2024-02-09 10:19:42 +0200

    1.1.1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+interface IKey {
+  getSignature(): string;
+}
+
+class Key implements IKey {
+  private signature: string = Math.random().toString(36).substring(2, 10);
+
+  getSignature(): string {
+    return this.signature;
+  }
+}
+
+interface IPerson {
+  getKey(): IKey;
+}
+
+class Person implements IPerson {
+  constructor(private key: IKey) {}
+
+  getKey(): IKey {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: IPerson[] = [];
+
+  constructor(protected key: IKey) {}
+
+  comeIn(person: IPerson): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: IKey): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: IKey): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +53,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
